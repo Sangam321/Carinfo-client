6ccf4ddde67015689b8fbc3244029a1443c61532
Sangam Basnet <sangambasnet1800@gmail.com>
2025-01-13 22:39:18 +0545

    login page finalization

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,67 +1,62 @@
-import COVER_IMAGE from '@/assets/logo.png';
-
-const colors = {
-    primary: '#606060',
-    background: '#f5f5f5',
-    disabled: '#D9D9D9',
-};
-
-const Login = () => {
+export function Login() {
     return (
-        <div className="w-full h-screen flex items-start">
-            <div className="relative w-1/2 h-full flex flex-col">
-                <div className="absolute top-[20%] left-[10%] flex flex-col">
-                    <h1 className="text-4xl text-white font-bold my-4">Turn Your Ideas into Reality</h1>
-                    <p className="text-xl text-white font-normal">Start for free and get attractive offers from the community</p>
+        <div class="py-6" style={{ paddingTop: "200px" }}> {/* Added padding to shift the card downward */}
+            <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
+                <div class="hidden lg:block lg:w-1/2 bg-cover"
+                    style={{
+                        backgroundImage:
+                            "url('https://w0.peakpx.com/wallpaper/269/590/HD-wallpaper-car-smoke-amoled-atomix-op-challenger-dodge-red-smoke-speed-thumbnail.jpg')",
+                    }}>
                 </div>
-                <img src={COVER_IMAGE} className="w-full h-full object-cover" />
-            </div>
+                <div class="w-full p-8 lg:w-1/2">
+                    <h2 class="text-3xl font-bold text-purple-700 text-center">Carinfo</h2>
+                    <p class="text-xl text-purple-600 text-center">Welcome back!</p>
 
-            <div className="w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center">
-                <h1 className="max-w-[500px] mx-auto text-xl text-[#606060] font-semibold mr-auto">Interactive Brand</h1>
-
-                <div className="w-full flex flex-col max-w-[500px]">
-                    <h3 className="text-3xl font-semibold mb-2">Login</h3>
-                    <p className="text-base mb-2">Welcome Back! Please enter your details.</p>
-
-                    <div className="w-full flex flex-col">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
-                        />
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
-                        />
+                    <div class="mt-4">
+                        <label class="block text-purple-700 text-sm font-bold mb-2">Email Address</label>
+                        <input class="bg-purple-200 text-purple-700 focus:outline-none focus:shadow-outline border border-purple-300 rounded py-2 px-4 block w-full appearance-none" type="email" />
                     </div>
 
-                    <div className="w-full flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input type="checkbox" className="w-4 h-4 mr-2" />
-                            <p className="text-sm">Remember me for 30 days</p>
+                    <div class="mt-4">
+                        <label class="block text-purple-700 text-sm font-bold mb-2">Password</label>
+                        <input class="bg-purple-200 text-purple-700 focus:outline-none focus:shadow-outline border border-purple-300 rounded py-2 px-4 block w-full appearance-none" type="password" />
+                        {/* Move "Forget Password?" below the password input */}
+                        <a href="#" class="text-xs text-purple-500 block mt-2 text-right">Forget Password?</a>
+                    </div>
+
+                    <div class="mt-8">
+                        <button class="bg-purple-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-purple-600">Login</button>
+                    </div>
+
+                    <a href="#" class="flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-purple-100">
+                        <div class="px-4 py-3">
+                            <svg class="h-6 w-6" viewBox="0 0 40 40">
+                                <path
+                                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                                    fill="#FFC107" />
+                                <path
+                                    d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                                    fill="#FF3D00" />
+                                <path
+                                    d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                                    fill="#4CAF50" />
+                                <path
+                                    d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                                    fill="#1976D2" />
+                            </svg>
                         </div>
-                        <p className="text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2">Forgot Password?</p>
-                    </div>
+                        <h1 class="px-4 py-3 w-5/6 text-center text-purple-600 font-bold">Sign in with Google</h1>
+                    </a>
 
-                    <div className="w-full flex flex-col my-4">
-                        <button className="w-full text-white my-2 font-semibold bg-[#606060] rounded-md p-4 text-center flex items-center justify-center">Log in</button>
-                        <button className="w-full text-[#606060] my-2 font-semibold bg-white border-2 border-black rounded-md p-4 text-center flex items-center justify-center">Register</button>
-                    </div>
-
-                    <div className="w-full flex items-center justify-center relative py-2">
-                        <div className="w-full h-[1px] bg-black"></div>
-                        <p className="absolute text-black/80 bg-[#f5f5f5]">or</p>
-                    </div>
-
-                    <div className="w-full flex items-center justify-center">
-                        <p className="text-sm font-normal text-[#606060]">Don't have an account? <span className="font-semibold underline underline-offset-2">Sign up</span></p>
+                    <div class="mt-4 flex items-center justify-between">
+                        <span class="border-b w-1/5 md:w-1/4"></span>
+                        <a href="#" class="text-xs text-purple-500 uppercase">or sign up</a>
+                        <span class="border-b w-1/5 md:w-1/4"></span>
                     </div>
                 </div>
             </div>
         </div>
     );
-};
+}
 
 export default Login;
